32ce4b26c1c532f21c5fa74c2ac29f9587b4cb2a
munozjuan <munoz.juan07@hotmail.com>
2020-11-24 00:33:14 -0500

    fixed function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 
-let requestURL = 'https://api.covidtracking.com/v1/us/current.json';
+/*let requestURL = 'https://api.covidtracking.com/v1/us/current.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+*/
 
+
+
+async function covidTracking() {
+
+    var queryURL = "https://api.covidtracking.com/v1/us/current.json";
+
+    var response = await $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        console.log(response);
+
+        var currentCovidNum = $("<div class='card-body' id='states'>");
+        var getCurrentNum = response.name;
+        
+        
+        var uvResponse = await $.ajax({
+            url: uvURL,
+            method: "GET"
+        })
+    }
