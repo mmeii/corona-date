d1ef6359b9a56eb82a970100c3b2b48166a62a30
munozjuan <munoz.juan07@hotmail.com>
2020-11-24 17:52:09 -0500

    fixed my function to work wtih json and added console.log to make sure there was an output.

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-
-/*let requestURL = 'https://api.covidtracking.com/v1/us/current.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-*/
+$(document).ready(function(){
+    //get JSON data from url
+    $.getJSON("https://api.covidtracking.com/v1/states/info.json", function(data){
+        var states = [];
 
 
 
-async function covidTracking() {
-
-    var queryURL = "https://api.covidtracking.com/v1/us/current.json";
-
-    var response = await $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        console.log(response);
-
-        var currentCovidNum = $("<div class='card-body' id='states'>");
-        var getCurrentNum = response.name;
-        
-        
-        var uvResponse = await $.ajax({
-            url: uvURL,
-            method: "GET"
-        })
+        console.log(data);
     }
+    )
+});
